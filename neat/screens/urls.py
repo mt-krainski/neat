0e777faac31e9286331fdb6324828fa88949c95c
Mateusz Krainski <mateusz@krainski.eu>
2022-07-31 11:50:25 -0400

    Start writing presenter screen
    Signed-off-by: Mateusz Krainski <mateusz@krainski.eu>

--- neat/screens/urls.py
+++ neat/screens/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main-screen/<int:presentation_id>/", views.index, name="index"),
+    path("main-screen/<int:slot_id>/", views.main_screen, name="index"),
+    path("presenter-screen/<int:slot_id>/", views.presenter_screen, name="index"),
     path("get-slot-page", views.get_slot_page, name="get-slot-page"),
 ]
--- neat/screens/views.py
+++ neat/screens/views.py
@@ -1,17 +1,25 @@
-from django.http import HttpResponseNotFound, JsonResponse
+from django.http import Http404, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
 from screens.models import PresentationSlot
 
 from presentations.models import Presentation
 
-TEMPLATES = {
-    Presentation.PresentationType.PDF: "screens/main-screen-pdf.html",
-    Presentation.PresentationType.EMBED: "screens/main-screen-embed.html",
-}
 
-# Create your views here.
-def index(request, presentation_id):
-    presentation = Presentation.objects.get(id=presentation_id)
+def _get_slot_by_slot_id(slot_id):
+    if not slot_id:
+        raise Http404("Missing slot parameter!")
+
+    try:
+        slot = PresentationSlot.objects.get(id=slot_id)
+    except PresentationSlot.DoesNotExist:
+        raise Http404("Presentation slot not found!")
+
+    return slot
+
+
+def main_screen(request, slot_id):
+    slot = _get_slot_by_slot_id(slot_id)
+    presentation = slot.presentation
     # TODO: make this more formal. For now we assume that there's only one slot
     #   per presentation
     slot = presentation.presentationslot_set.first()
@@ -23,15 +31,20 @@ def index(request, presentation_id):
     return render(request, "screens/main-screen.html", context)
 
 
+def presenter_screen(request, slot_id):
+    slot = _get_slot_by_slot_id(slot_id)
+    presentation = slot.presentation
+    # TODO: make this more formal. For now we assume that there's only one slot
+    #   per presentation
+    slot = presentation.presentationslot_set.first()
+    context = {
+        "slot": slot,
+        "presentation": presentation,
+        "presentation_type": presentation.get_type_display(),
+    }
+    return render(request, "screens/presenter-screen.html", context)
+
+
 def get_slot_page(request):
-    slot_id = request.GET.get("slot_id")
-
-    if not slot_id:
-        return HttpResponseNotFound("Missing slot parameter!")
-
-    try:
-        slot = PresentationSlot.objects.get(id=slot_id)
-    except PresentationSlot.DoesNotExist:
-        return HttpResponseNotFound("Presentation slot not found!")
-
+    slot = _get_slot_by_slot_id(request.GET.get("slot_id"))
     return JsonResponse({"current_page": slot.current_page})
